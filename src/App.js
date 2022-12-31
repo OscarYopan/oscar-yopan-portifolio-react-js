01eb62942ff8255efdd71442493e38d4307af394
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-31 11:45:25 -0300

    Portifiolio Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About/about'
 import Header from './components/Header/Header.jsx'
 import Home from './components/Home/home.jsx'
+import Portifolio from './components/Portifolio/portifolio.jsx'
 import Skills from './components/Skills/skills.jsx'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Portifolio />
     </div>
   )
 }
---
+++ src/components/Portifolio/portifolio.jsx
@@ -0,0 +1,15 @@
+import './portifolio.css'
+
+function Portifolio() {
+  return(
+    <div className='Portifolio'>
+      <div class="portifolio" id="portifolio">
+        <h1 class="title">Portifolio</h1>
+        <p class="description">This is my personal portifolio, if you prefer, go to my <a href="https://github.com/OscarYopan" target="_blank" rel="noreferrer">Github</a> profile to see more.</p>    
+        <div class="portifolio-container"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Portifolio
