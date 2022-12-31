c4b7ffa397a1539163718230050d979397309feb
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-31 10:54:26 -0300

    --

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
+import Home from './components/Home/home.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   )
 }
