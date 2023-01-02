4d7740a6fdb6f1bb6473c7a133a703ee7dd1708b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-02 09:04:58 -0300

    --

--- src/components/Swipper/swipper.jsx
+++ src/components/Swipper/swipper.jsx
@@ -1,21 +1,5 @@
 import './swipper.css'
 import './swipper.js'
-import 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js'
-import 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js'
-
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import "./styles.css";
-
-// import required modules
-import { Pagination, Navigation } from "swiper";
 
 function Swipper() {
   return (
