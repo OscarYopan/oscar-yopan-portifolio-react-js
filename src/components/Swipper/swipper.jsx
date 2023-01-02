cd561ab28cc3c7bb90ff225d12877f8e7391ef3d
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-02 09:28:53 -0300

    --

--- src/components/Swipper/swipper.jsx
+++ src/components/Swipper/swipper.jsx
@@ -1,16 +1,41 @@
-import './swipper.css'
-import './swipper.js'
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function Swipper() {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function App() {
   return (
-    <div className='Swipper'>
-      {/* <!-- Swiper --> */}
-      <div class="swiper mySwiper">
-        <div class="swiper-wrapper"></div>
-        <div class="swiper-pagination"></div>
-      </div>
-    </div>
-  )  
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
-
-export default Swipper
