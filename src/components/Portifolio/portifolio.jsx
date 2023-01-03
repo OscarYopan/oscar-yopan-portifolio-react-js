25d7007c8ce6195266c47e36cfb7c4a7eb399e1e
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-03 09:27:53 -0300

    --

--- src/components/Portifolio/portifolio.jsx
+++ src/components/Portifolio/portifolio.jsx
@@ -6,8 +6,11 @@ function Portifolio() {
       <div className="portifolio" id="portifolio">
         <h1 className="title">Portifolio</h1>
         <p className="description">This is my personal portifolio, if you prefer, go to my <a href="https://github.com/OscarYopan" target="_blank" rel="noreferrer">Github</a> profile to see more.</p>    
-        <div className="portifolio-container"></div>
-        <iframe src="https://oscaryopan.github.io/swiper-portifilio-pages/" frameborder="0" title='projects'></iframe>
+        <div className="portifolio-container">
+          <iframe src="https://oscaryopan.github.io/swiper-portifilio-pages/" frameborder="0" title='projects'>
+            <p>Your browser does not support iframes.</p>
+          </iframe>
+        </div>
       </div>
     </div>
   )
