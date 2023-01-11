9b47c8392156cd83e8018c22b08d089e5ef157fb
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-11 14:48:01 -0300

    --

---
+++ src/App2.js
@@ -0,0 +1,11 @@
+import MobileMenu from './components/mobile/mobileMenu'
+
+function App() {
+  return (
+    <div className="App">
+      <MobileMenu />
+    </div>
+  )
+}
+
+export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,23 +15,9 @@ function Header() {
             <li><a href="#skills">skills</a></li>
             <li><a href="#portifolio">Portifolio</a></li>
           </ul>
-          <div className='mobile-container'>
-            <button><i class="fa-solid fa-bars"></i></button>
-          </div>
         </div>
       </nav>
     </header>
-    <div className='mobile-menu close'>
-      <button><i class="fa-solid fa-xmark"></i></button>
-      <ul>
-        <li>
-          <a href="#home"><i class="fa-solid fa-house"></i></a>
-          <a href="#about"><i class="fa-solid fa-circle-info"></i></a>
-          <a href="#skilss"><i class="fa-solid fa-screwdriver-wrench"></i></a>
-          <a href="#portiolio"><i class="fa-solid fa-gallery-thumbnails"></i></a>
-        </li>
-      </ul>
-    </div>
   </div>
 }
 
