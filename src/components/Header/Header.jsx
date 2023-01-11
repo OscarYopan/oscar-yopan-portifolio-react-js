3c65b4f26739b4e580d171fb65c91e5b0aca89a3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-11 14:28:23 -0300

    --

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
           <div id="logo">
             <h3 className="logo"><a href="index.html">Oscar Yopan</a></h3>
           </div>
-          <ul className="menu open">
+          <ul className="menu">
             <li><a href="#home" className="current">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#skills">skills</a></li>
@@ -17,11 +17,21 @@ function Header() {
           </ul>
           <div className='mobile-container'>
             <button><i class="fa-solid fa-bars"></i></button>
-            <button><i class="fa-solid fa-xmark"></i></button>
           </div>
         </div>
       </nav>
     </header>
+    <div className='mobile-menu close'>
+      <button><i class="fa-solid fa-xmark"></i></button>
+      <ul>
+        <li>
+          <a href="#home"><i class="fa-solid fa-house"></i></a>
+          <a href="#about"><i class="fa-solid fa-circle-info"></i></a>
+          <a href="#skilss"><i class="fa-solid fa-screwdriver-wrench"></i></a>
+          <a href="#portiolio"><i class="fa-solid fa-gallery-thumbnails"></i></a>
+        </li>
+      </ul>
+    </div>
   </div>
 }
 
