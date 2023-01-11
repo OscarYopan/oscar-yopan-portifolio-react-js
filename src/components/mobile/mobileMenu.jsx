bb295bec46b29c2370c15c01163b06923834223f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-11 14:56:54 -0300

    --

--- src/components/mobile/mobileMenu.jsx
+++ src/components/mobile/mobileMenu.jsx
@@ -6,10 +6,8 @@ function MobileMenu() {
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </head>
-      <input type="checkbox" id="menu-toggle"/>
-      <label id="trigger" for="menu-toggle"></label>
-      <label id="burger" for="menu-toggle"></label>
-      <ul id="menu">
+      <button className='toggle'><i class="fa-solid fa-bars"></i></button>
+      <ul id="mobile-menu">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skilss">Skills</a></li>
