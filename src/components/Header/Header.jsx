e0602f311471e57e0f7e9a048a60b692fc6598ce
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-13 10:50:28 -0300

    --

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import './Header.css'
-import './header.js'
 
 function Header() {
   return <div className="header">
@@ -9,7 +8,7 @@ function Header() {
           <div id="logo">
             <h3 className="logo"><a href="index.html">Oscar Yopan</a></h3>
           </div>
-          <div  className="menu">
+          <div  className="menu open">
             <ul>
               <li><a href="#home" className="current">Home</a></li>
               <li><a href="#about">About</a></li>
@@ -17,11 +16,29 @@ function Header() {
               <li><a href="#portifolio">Portifolio</a></li>
             </ul>
           </div>
-          <button className='toggle'><i class="fa-solid fa-bars"></i></button>
+          <button className='toggle' onClick={openMenu}><i class="fa-solid fa-bars"></i></button>
         </div>
       </nav>
     </header>
   </div>
 }
+
+// ************ Open Mobile Menu ************
+function openMenu() {
+  const menu = document.querySelector('.menu')
+  menu.classList.toggle('open')
+}
+
+// ************ Nav Animation ***************
+// function fixNav() {
+//   const nav = document.querySelector('.nav')
+//   window.addEventListener('scroll', fixNav)
+//   if (window.scrollY > nav.offsetHeight + 150) {
+//     nav.classList.add('active')
+//     console.log(nav.offsetHeight)
+//   } else {
+//     nav.classList.remove('active')
+//   }
+// }
 
 export default Header
