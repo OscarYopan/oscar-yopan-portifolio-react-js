5641b085c5098a099cb35f4ecff39315586d38cc
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-14 10:45:20 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,6 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
-  // ************** Close Menu on Click *****************
-  React.useEffect(() => {
-    const links = document.querySelectorAll('menu ul li a')
-    links.addEventListener('click', closeMenuOnClick)
-    function closeMenuOnClick() {
-      const menu = document.querySelector('.menu')
-      for (const link of links) {
-        link.addEventListener('click', () => {
-          menu.classList.remove('open')
-        })
-      }
-    }
-  }, []);
-
   return <div className="header">
     <header className="header" id="header">
       <nav className="nav">
@@ -43,6 +29,18 @@ function openMenu() {
   const menu = document.querySelector('.menu')
   menu.classList.toggle('open')
 }
+
+// ************** Close Menu on Click *****************
+// function closeMenuOnClick() {
+//   const links = document.querySelectorAll('menu ul li a')
+//   links.addEventListener('click', closeMenuOnClick)
+//   const menu = document.querySelector('.menu')
+//   for (const link of links) {
+//     link.addEventListener('click', () => {
+//       menu.classList.remove('open')
+//     })
+//   }
+// }
 
 // ************** Nav Animation ***********************
 // function fixNav() {
