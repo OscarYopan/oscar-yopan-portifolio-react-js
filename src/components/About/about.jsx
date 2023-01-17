88b15847174cf09bfcb26d0f41cbe7bfde5170fc
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-17 14:10:24 -0300

    --

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -13,9 +13,10 @@ function About() {
               <p className="presentation">I have been working in another area for the last 11 years, but that was never my dream and that was an a frustation for me. Since I discover the developing, I feel mysel realized, in many times during my studies and pratices, feel not working or studing, I'm having fun!</p>
               <button className="button">
                 <a 
-                href="../img/resume.pdf"
+                href="https://raw.githubusercontent.com/OscarYopan/oscar-yopan-portifolio-react-js/main/src/components/img/resume.pdf"
                 target="_blank" 
-                download="ResumeOscarYopan">
+                download="ResumeOscarYopan.pdf" 
+                rel="noreferrer">
                   Dowload CV
                 </a>
               </button>
