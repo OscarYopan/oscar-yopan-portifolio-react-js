65d36ef6e79da4c8247d591d2e036cd729c34094
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-01-17 13:53:49 -0300

    --

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -11,7 +11,14 @@ function About() {
             <div className="oscar-photo"></div>
             <div className="me">
               <p className="presentation">I have been working in another area for the last 11 years, but that was never my dream and that was an a frustation for me. Since I discover the developing, I feel mysel realized, in many times during my studies and pratices, feel not working or studing, I'm having fun!</p>
-              <button className="button">Dowload CV</button>
+              <button className="button">
+                <a 
+                href="../img/resume.pdf"
+                target="_blank" 
+                download="ResumeOscarYopan">
+                  Dowload CV
+                </a>
+              </button>
             </div>
           </div>
 
